6a7b50a65d4f5d461e92de888f7d74c61d478e1c
Adrian <adrian@adrianthedev.com>
2022-08-06 18:10:17 +0300

    dark mode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './docs/**/*.{html,js,vue,ts,md}',
     './docs/.vitepress/**/*.{html,js,vue,ts,md}',
