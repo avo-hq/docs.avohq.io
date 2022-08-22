1a9e57dfdb4a89bfc0fdafda56ebff6069e9c188
Adrian <adrian@adrianthedev.com>
2022-08-22 16:57:59 +0300

    add customizable controls docs

---
+++ docs/.vitepress/theme/components/Edit.vue
@@ -0,0 +1,3 @@
+<template>
+  <a href="/2.0/resources.html#edit"><code>Edit</code></a>
+</template>
---
+++ docs/.vitepress/theme/components/New.vue
@@ -0,0 +1,3 @@
+<template>
+  <a href="/2.0/resources.html#new"><code>New</code></a>
+</template>
