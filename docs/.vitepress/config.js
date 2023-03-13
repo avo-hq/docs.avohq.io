10b0dd20e849bd91157ab7c7cb4cac953b6b835f
Adrian <adrian@adrianthedev.com>
2023-03-13 11:30:12 +0200

    add dynamic filters to avo 3

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -145,10 +145,18 @@ const config = {
             {text: "Grid view", link: "/3.0/grid-view"},
             {text: "Menu editor", link: "/3.0/menu-editor"},
             {text: "Search", link: "/3.0/search"},
-            {text: "Filters", link: "/3.0/filters"},
             {text: "Actions", link: "/3.0/actions"},
             {text: "Localization (I18n)", link: "/3.0/localization"},
             {text: "Branding", link: "/3.0/branding"},
+          ],
+        },
+        {
+          text: "Filters",
+          items: [
+            {text: "Overview", link: "/3.0/filters"},
+            {text: "Basic Filters", link: "/3.0/filters/basic"},
+            {text: "Dynamic filters", link: "/3.0/filters/dynamic-filters"},
+            {text: "Advanced filters", link: "/3.0/filters/advanced-filters"},
           ],
         },
         {
