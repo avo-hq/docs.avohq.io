2e192d34c3d33ca404dd203d8b6983345f9545e2
Adrian <adrian@adrianthedev.com>
2023-03-13 13:42:22 +0200

    optimize option

--- docs/.vitepress/theme/components/Option.vue
+++ docs/.vitepress/theme/components/Option.vue
@@ -3,11 +3,13 @@ export default {
   props: ['name'],
   setup(props, {slots}) {
     const name = (props.name || "");
-    const anchorName = `#${name}`
+    const anchor = name.replace(/\?|{|}|!/g, '')
+    const anchorName = `#${anchor}`
 
     // expose to template and other options API hooks
     return {
       name,
+      anchor,
       anchorName
     }
   },
@@ -15,7 +17,7 @@ export default {
 </script>
 
 <template>
-  <h2 :id="name" tabindex="-1"><code><span class="hidden">-> </span><span v-html="name" /></code><a class="header-anchor" :href="anchorName" aria-hidden="true">#</a></h2>
+  <h2 :id="anchor" tabindex="-1"><code><span class="hidden">-> </span><span v-html="name" /></code><a class="header-anchor" :href="anchorName" aria-hidden="true">#</a></h2>
   <div class="pl-8">
     <slot></slot>
   </div>
