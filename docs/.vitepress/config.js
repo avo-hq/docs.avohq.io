08de9b552b59ed1c3811587572120160b651d663
Adrian <adrian@adrianthedev.com>
2023-06-27 20:52:48 +0300

    add google analyticcs 4

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -40,12 +40,12 @@ const config = {
     // <script async="" src="https://www.google-analytics.com/analytics.js"></script>
     [
       'script',
-      { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=UA-174545089-1' }
+      { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-VWMV2FNBQ1' }
     ],
     [
       'script',
       {},
-      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'UA-174545089-1');"
+      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-VWMV2FNBQ1');"
     ],
     [
       'script',
