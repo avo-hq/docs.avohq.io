f1133ed738a1dfb9358e52e9bcd644f5f7f46f95
Adrian <adrian@adrianthedev.com>
2023-07-04 23:21:27 +0300

    automatic recipes

---
+++ docs/.vitepress/recipes.data.js
@@ -0,0 +1,20 @@
+import {getFiles} from "./getFiles"
+
+const replaceExtension = (i) => {
+  return {
+    ...i,
+    link: i.link.replace('.md', '.html')
+  }
+}
+
+export default {
+  load() {
+    const v2 = getFiles('recipes', '2.0').map((i) => replaceExtension(i))
+    const v3 = getFiles('recipes', '3.0').map((i) => replaceExtension(i))
+
+    return {
+      v2,
+      v3
+    }
+  }
+}
