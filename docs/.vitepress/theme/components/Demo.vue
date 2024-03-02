9787779f95003e3f54afbf8cdf42e01c818535b7
Adrian <adrian@adrianthedev.com>
2024-03-02 14:23:12 +0700

    address feedback

--- docs/.vitepress/theme/components/Demo.vue
+++ docs/.vitepress/theme/components/Demo.vue
@@ -11,7 +11,7 @@ const props = defineProps({
   <a :href="link"
     target="_blank"
     v-if="link"
-    class="text-sm inline-flex items-center rounded hite no-underline px-2 py-1 leading-none border dark:border-white border-gray-900 hover:bg-gray-900 !text-gray-900 dark:!text-white hover:!text-white dark:hover:bg-white dark:hover:!text-gray-900"
+    class="text-sm inline-flex items-center rounded !no-underline px-2 py-1 leading-none border dark:border-white border-gray-900 hover:bg-gray-900 !text-gray-900 dark:!text-white hover:!text-white dark:hover:bg-white dark:hover:!text-gray-900"
   >
     <WrenchScrewdriverIcon class="h-4 inline mr-1 dark:stroke-white hover:dark:stroke-white" /> Experience the demo
   </a>
