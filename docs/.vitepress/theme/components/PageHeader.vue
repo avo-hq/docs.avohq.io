02e95c778653570ac0cc2ce125b59eb7a87412fc
Adrian <adrian@adrianthedev.com>
2025-06-21 15:59:02 +0300

    add add_on name for the savvycal link

--- docs/.vitepress/theme/components/PageHeader.vue
+++ docs/.vitepress/theme/components/PageHeader.vue
@@ -13,6 +13,7 @@ const version = computed(() => frontmatter.value.version)
 const demoVideo = computed(() => frontmatter.value.demoVideo)
 const demo = computed(() => frontmatter.value.demo)
 const betaStatus = computed(() => frontmatter.value.betaStatus)
+const add_on = computed(() => frontmatter.value.add_on)
 
 const shouldHide = computed(() => [feedbackId, license, version, demoVideo, demo, betaStatus].every((i) => i.value === undefined))
 </script>
@@ -21,7 +22,7 @@ const shouldHide = computed(() => [feedbackId, license, version, demoVideo, demo
   <div class="grid grid-cols-2 gap-2 mb-6" v-if="!shouldHide">
     <VersionReq :version="version" v-if="version" />
     <FeedbackPill :feedback-id="feedbackId" v-if="feedbackId" />
-    <LicenseReq :license="license" v-if="license" />
+    <LicenseReq :license="license" :add_on="add_on" v-if="license" />
     <DemoVideo :demo-video="demoVideo" v-if="demoVideo" />
     <Demo :link="demo" v-if="demo" />
     <BetaStatus :label="betaStatus" v-if="betaStatus" />
