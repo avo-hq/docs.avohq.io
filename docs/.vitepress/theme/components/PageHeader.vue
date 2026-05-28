f025c0b9604ab7d60d0ffc431b92075904755ab8
Adrian <adrian@adrianthedev.com>
2026-05-28 11:18:49 +0300

    wip

--- docs/.vitepress/theme/components/PageHeader.vue
+++ docs/.vitepress/theme/components/PageHeader.vue
@@ -47,13 +47,13 @@ const shouldHide = computed(() => [feedbackId, license, version, demoVideo, demo
     <div class="api-docs-callout__icon" aria-hidden="true">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
         stroke-linecap="round" stroke-linejoin="round">
-        <line x1="19" y1="12" x2="5" y2="12" />
-        <polyline points="12 19 5 12 12 5" />
+        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
       </svg>
     </div>
     <div class="api-docs-callout__body">
       <span class="api-docs-callout__title">Task-oriented docs and worked examples</span>
-      <span class="api-docs-callout__subtitle">&larr; See the guides</span>
+      <span class="api-docs-callout__subtitle">See the guides &rarr;</span>
     </div>
   </a>
   <div class="hidden">
