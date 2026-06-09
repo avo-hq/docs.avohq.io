5d831e1bb38345d42690bee59da556f19e868f4a
Adrian <adrian@adrianthedev.com>
2026-06-09 22:05:49 +0300

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
+    // VitePress 2.x ships theme CSS that uses native CSS nesting (e.g. `&:lang(ja)`).
+    // Flatten it before Tailwind runs, otherwise PostCSS warns that nesting isn't
+    // configured. https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
